170b22fbdf11fc9eaa76109f1df614d8544ec6a1
Alexander <alfonso.ruesta@unmsm.edu.pe>
2019-05-28 22:45:33 -0500

    Database

--- Back/database/migrations/2019_05_29_010913_create_programmings_table.php
+++ Back/database/migrations/2019_05_29_010913_create_programmings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProgrammingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('programmings', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+
+            $table->integer('teacher_id')->unsigned();
+            
+            $table->integer('hour');
+            $table->string('classroom');
+
+
+            $table->foreign('teacher_id')->references('id')->on('teachers');
         });
     }
 
@@ -31,6 +34,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('programmings');
     }
 }
